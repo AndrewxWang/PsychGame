33aec31feb026fc4fffe245a64a9f320fe4e5883
Andrew Wang <57112250+AndrewxWang@users.noreply.github.com>
2022-05-19 23:45:37 -0400

    CHANGES

---
+++ files/questions.js
@@ -0,0 +1,69 @@
+var questionsList = [
+  {
+    //research unit
+    Question: "Bob performed an experiment on the relationship between sleep and productivity. Bob's results showed that as sleep went up, productivity went up. Which of the following is NOT an accurate conclusion?",
+    A: "Sleep and Productivity have a positive correlation",
+    B: "Poor sleep causes a decrease in productivity",
+    C: "Sleep and Productivity have a negative correlation",
+    D: "No conclusion can be drawn from his data",
+    ANS: "B",
+  },
+  {
+    Question: "John wants to perform an experiment on children, but does not give them full details of the experiment. Which of the following ethical laws is John violating?",
+    A: "Deception",
+    B: "Informed consent",
+    C: "Debrief",
+    D: "Protection of Participants",
+    ANS: "A",
+  },
+  {
+    Question: "Which of the following components of an experiment is used in order to control the confounding variables?",
+    A: "Random sampling",
+    B: "Operational definitions",
+    C: "Debriefing",
+    D: "Random assignment",
+    ANS: "D",
+  },
+  {
+    Question: "Which of the following best explains why researchers define operational definitions?",
+    A: "To following ethical laws of Psychology",
+    B: "To make sure the participants are unaware of the experimental group",
+    C: "To ensure that the experiment is reproducable ",
+    D: "To see how a placebo can impact the results of the control group",
+    ANS: "C",
+  },
+
+  //social psych unit
+  {
+    Question: "Christina begins to like rap music after listening to it on the radio for numerous days. Which of the following explains the reason for Christina to like rap?",
+    A: "Serial position effect",
+    B: "Mere exposure effect",
+    C: "Halo effect",
+    D: "Just world phenomenon",
+    ANS: "B",
+  },
+  {
+    Question: "After acing a test, Randy begins to flaunt how smart and intelligent he is. However, after getting a 0, he blames his teacher and school. Which of the following explains Randy's behavior",
+    A: "Fundamental attribution error",
+    B: "Social Loafing",
+    C: "Ingroup/Outgroup bias",
+    D: "Self-serving bias",
+    ANS: "D",
+  },
+  {
+    Question: "The tendency to act reserved and cautious in group situations while discussing conflict in order to maintain harmony is known as",
+    A: "Social facilitation",
+    B: "Social loafing",
+    C: "Groupthink",
+    D: "Group polarization",
+    ANS: "C",
+  },
+  {
+    Question: "Eric is a reserved and shy student. He tries to be nice to people around him and respects his teachers. However, online, Eric rants on social media and is critical to almost everyone. Which of the following explains Eric's change in behavior?",
+    A: "Group polarization",
+    B: "Deindividualization",
+    C: "Just world phenomenon",
+    D: "Halo effect",
+    ANS: "B",
+  },
+];
--- script.js
+++ script.js
@@ -1,38 +1,80 @@
 var count = 1;
-var unitList = [researchQuestions, socialPsychQuestions];
-var unitQuestions;
+var score = 0;
 var randomNum;
+var questionsUsed = [];
 
-function createPrompt(unitQuestions) {
+function createPrompt() {
     document.getElementById("questionNum").innerHTML = "Question " + count + ":";
-    document.getElementById("question").innerHTML = unitQuestions[randomNum]["Question"];
-    document.getElementById("A").innerHTML = "A. " + unitQuestions[randomNum]["A"];
-    document.getElementById("B").innerHTML = "B. " + unitQuestions[randomNum]["B"];
-    document.getElementById("C").innerHTML = "C. " + unitQuestions[randomNum]["C"];
-    document.getElementById("D").innerHTML = "D. " + unitQuestions[randomNum]["D"];
+    document.getElementById("question").innerHTML = questionsList[randomNum]["Question"];
+    document.getElementById("A").innerHTML = "A. " + questionsList[randomNum]["A"];
+    document.getElementById("B").innerHTML = "B. " + questionsList[randomNum]["B"];
+    document.getElementById("C").innerHTML = "C. " + questionsList[randomNum]["C"];
+    document.getElementById("D").innerHTML = "D. " + questionsList[randomNum]["D"];
 }
 
 function checkAns(letter) {
-    if (letter == unitQuestions[randomNum]["ANS"]) {
+    if (letter == questionsList[randomNum]["ANS"]) {
         console.log("correct");
         count++;
+        score++;
         randomize();
-        createPrompt(unitQuestions);
+        changeScore(score);
+        createPrompt(questionsList);
     } else {
         console.log("wrong");
     }
 }
 
+function changeScore(score) {
+    scaleUp("currScore");
+    document.getElementById("currScore").innerHTML = "Score: " + score;
+    if (score > Number(localStorage.highScore)) {
+        localStorage.highScore = score;
+        scaleUp("highscore");
+        document.getElementById("highscore").innerHTML = "High Score: " + localStorage.highScore;
+    }
+}
+
 function randomize() {
-    unitQuestions = unitList[Math.floor(Math.random() * unitList.length)];
-    randomNum = Math.floor(Math.random() * unitQuestions.length);
+    randomNum = Math.floor(Math.random() * questionsList.length);
+    if (questionsUsed.includes(randomNum)){
+        var sentinel = 0;
+        while (questionsUsed.includes(randomNum)){
+            randomNum = Math.floor(Math.random() * questionsList.length);
+            sentinel++;
+            if (sentinel == 999){
+                alert("DONE");
+                break;
+            }
+        }
+    }
+    questionsUsed.push(randomNum);
+}
+
+
+//animation
+function scaleUp(myId) {
+    document.getElementById(myId).style.fontSize = "5vh";
+    setTimeout(function () {
+        document.getElementById(myId).style.fontSize = "3vh";
+    }, 250);
 }
 
 function loadSite(){
+    if (!localStorage.highScore) {
+        localStorage.highScore = 0;
+    }
+
+    if (!localStorage.clickLink) {
+        localStorage.clickLink = 0;
+    }
+    document.getElementById("highscore").innerHTML = "High Score: " + localStorage.highScore;
+    document.getElementById("currScore").innerHTML = "Score: " + score;
+    
     randomize();
-    createPrompt(researchQuestions);
+    createPrompt();
     
     setTimeout(function(){
-        document.getElementById("prompt").style.transform = "translateY(-7.5vh)";
+        document.getElementById("prompt").style.transform = "translateY(-5.5vh)";
     },300);
 }
